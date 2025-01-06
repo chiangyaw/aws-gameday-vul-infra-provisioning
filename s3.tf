e341b32a3ad1d9f4bcfbf6a9b7227900d678d8b9
chiangyaw <chiangyaw@gmail.com>
2025-01-06 15:30:27 +0800

    initial commit

--- s3.tf
+++ s3.tf
@@ -5,3 +5,9 @@ resource "aws_s3_bucket" "gameday_s3_bucket"{
         Environment = "Prod"
     }
 }
+
+resource "aws_s3_object" "sensitive_data_s3_object" {
+  key    = "sensitive-data-s3-object"
+  bucket = aws_s3_bucket.gameday_s3_bucket.id
+  source = "1-MB-Test-SensitiveData.xlsx"
+}
